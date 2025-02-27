b53f2d9545404f0b9a882e59b3e225d8304b17cf
John Liton Mardy <mardy.cse@gmail.com>
2025-02-28 01:38:53 +0600

    practice set 1

---
+++ practice_set_1.js
@@ -0,0 +1,20 @@
+//Chaptar 1 Practice set
+// 1. Create a variable of type String and try to add a number to it
+
+let data1 = 'Data';
+console.log(data1);
+let data = 10;
+console.log(data)
+//Concatination
+console.log(data1+data)
+//Find the type
+console.log(typeof(data1+data))
+
+const item = {
+    'id':1,
+    'name':"Liton",
+}
+console.log(item);
+item.id = 10;
+item.lastName = "Mardy";
+console.log(item);
