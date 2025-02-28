61bbc83b1a68148c4af4cb3a8d69763e6ddaec46
John Liton Mardy <mardy.cse@gmail.com>
2025-02-28 12:05:47 +0600

    expression and operator

---
+++ expression_and_oparator.js
@@ -0,0 +1,33 @@
+const topic =  "Condition, Oparator, expression in JS"
+console.log(topic)
+
+//Arithmetic Operator
+const arithmeticOperator = {
+    addition : '+',
+    subtraction : '-',
+    multiplication : '*',
+    exponentiation : '**',
+    division : '/',
+    modulus : '%',
+    increment : '++',
+    decrement : '--',
+}
+console.log(arithmeticOperator)
+
+
+let a = 45;
+let b = 4;
+
+console.log("a+b = ",a+b);
+console.log("a-b = ",a-b);
+console.log("a*b = ",a*b);
+console.log("a/b = ",a/b);
+console.log("a%b = ",a%b);
+console.log("a**b = ",a**b);
+console.log("a++ = ",a++); //print 45 
+console.log("a++ = ",a); //print 46
+console.log("a++ = ",a--); //print 46
+console.log("a++ = ",a); //print 45
+console.log("a++ = ",--a); //print 44
+console.log("a++ = ",++a); //print 45
+
